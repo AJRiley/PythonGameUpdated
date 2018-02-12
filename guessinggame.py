9b0958930ccd3f165b5cd3e17fb698e4a07af3d8
AJ Riley <ajriley774@gmail.com>
2018-02-12 21:40:01 +0000

    Create guessinggame.py

---
+++ guessinggame.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import random
+
+number = random.randint(1, 10)
+tries = 1
+win = False
+
+
+name = input("Hello, What is your username?")
+
+print("Hello", name + ".", )
+
+question = input("Would you like to play a game? [Y/N] ")
+if question == "n":
+    print("oh..okay")
+
+if question == "y":
+    print("I'm thinking of a number between 1 & 10")
+while not win:
+    guess = int(input("Have a guess: "))
+    tries = tries + 1
+    if guess == number:
+        win = True
+    elif guess < number:
+        print("Guess Higher")
+    elif guess > number:
+        print("Guess Lower")
+if win:
+    print("Winner")
